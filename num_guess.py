ff15d4da3052a25da8260a9267d845739c9f08fa
Michael Holley <michael@pop-os.localdomain>
2021-12-04 17:27:25 -0500

    added future plans and notes for num_guess

---
+++ num_guess.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+'''
+Things we want in this program
+3 guesses until given the answer
+Formatting to make the game easier on the eyes
+Ascii art When the game is ran
+'''
+
